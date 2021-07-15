fd2bfe845631f2e202d4b701f280a31cb68cc6e5
Bergers373 <83513825+Bergers373@users.noreply.github.com>
2021-07-15 21:38:18 +0800

    Update teams_profile1.php

--- teams_profile1.php
+++ teams_profile1.php
@@ -198,7 +198,7 @@
 													</div>
 													<div class="modal-footer">
 														<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-														<button type="submit" class="btn btn-primary">Save Data</button>
+														<button type="submit" name="updatedata" class="btn btn-primary">Save Data</button>
 													</div>
 												</div>
 											</div>
